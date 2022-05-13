4ba169e18ed81b661cb65fa41d5361eff5a18f63
IgnFed <ignaciofedorenco2317@gmail.com>
2022-05-13 12:04:52 -0300

    Revert "Plantilla para la aplicación"
    This reverts commit c11a21cb465feead97840fee86a6fa89c4891d97.

--- kimchechallenge/src/index.js
+++ kimchechallenge/src/index.js
@@ -1,13 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import { Wrapper } from './components/Wrapper';
 
 ReactDOM.render(
 	<React.StrictMode>
-		<Wrapper>
-			<App />
-		</Wrapper>
+		<App />
 	</React.StrictMode>,
 	document.getElementById('root')
 );
