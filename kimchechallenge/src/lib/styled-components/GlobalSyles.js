71a2ce3711d3103fab379f0e03c93ccbe520e347
IgnFed <ignaciofedorenco2317@gmail.com>
2022-05-13 16:14:54 -0300

    Estilos globales modificados

--- kimchechallenge/src/lib/styled-components/GlobalSyles.js
+++ kimchechallenge/src/lib/styled-components/GlobalSyles.js
@@ -6,11 +6,23 @@ export default createGlobalStyle`
 		padding: 0;
 		margin: 0;
 	}
-	button, input {
-		padding: .75rem;
+
+	body {
 		color: ${props => (props.theme.mode === 'dark' ? '#ffffff' : '#000000')};
+	}
+
+	button, input, select {
+		padding: .75rem;
 		border: none;
 		outline: none;
+		background-color: ${props =>
+			props.theme.mode === 'light' ? '#9395bd' : '#393b59'};
 		border-radius: .475rem
+	}
+	.active{
+		background-color: red;
+	}
+	input {
+		width: 100%
 	}
 `;
