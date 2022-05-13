409cf0e3e5e9f83e7224681bbecb52081d72e2e3
IgnFed <ignaciofedorenco2317@gmail.com>
2022-05-13 12:02:58 -0300

    Estilos para dark y light mode

---
+++ kimchechallenge/src/lib/styled-components/themes.js
@@ -0,0 +1,19 @@
+export const lightTheme = {
+	outline: {
+		$hover: '#1b74fa',
+	},
+	colors: {
+		background: '#f0f0f7',
+		text: '#000000',
+	},
+};
+
+export const darkTheme = {
+	outline: {
+		$hover: '#1bcdfa',
+	},
+	colors: {
+		background: '#1d1f38',
+		text: '#ffffff',
+	},
+};
