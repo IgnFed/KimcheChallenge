ad15a39fa950a3de825c07d8647f2db769183593
IgnFed <ignaciofedorenco2317@gmail.com>
2022-05-13 16:19:55 -0300

    Revert "Estilos de input eliminados"
    This reverts commit a37eb0ae3773169bcdd2607111e63b9296536083.

--- kimchechallenge/src/components/Input/index.component.jsx
+++ kimchechallenge/src/components/Input/index.component.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
-import StyledInput from './index.styled';
 
 export const Input = ({ value, onChange, placeholder }) => (
-	<StyledInput onChange={onChange} value={value} placeholder={placeholder} />
+	<input onChange={onChange} value={value} placeholder={placeholder} />
 );
