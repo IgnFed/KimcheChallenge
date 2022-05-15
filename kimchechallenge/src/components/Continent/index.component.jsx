1d3fd5e44a7488bd2b765df3818b3bedf38bb096
IgnFed <ignaciofedorenco2317@gmail.com>
2022-05-15 20:06:29 -0300

    Component to render a list of cards

---
+++ kimchechallenge/src/components/Continent/index.component.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import { Card } from '../../components/Card/index.component';
+
+export const ContinentsList = ({ continents = [] }) => {
+	console.log(continents);
+	return <></>;
+};
