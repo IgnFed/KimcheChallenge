02591660e20112af8ac21b4e21756566835765f3
IgnFed <ignaciofedorenco2317@gmail.com>
2022-05-15 20:07:37 -0300

    File structure has been radically changed

--- kimchechallenge/src/App.jsx
+++ kimchechallenge/src/App.jsx
@@ -1,12 +1,12 @@
-import React, { Fragment, useEffect, useReducer, useState } from 'react';
+import React, { useEffect, useReducer, useState } from 'react';
 import { useQuery } from '@apollo/react-hooks';
-import { GET_COUNTRIES } from './lib/graphql/getCountries';
+import { GET_CONTINENTS } from './lib/graphql';
 import { Container } from './components/Container/index.component';
 import { Button } from './components/Button/index.component';
 import { Input } from './components/Input/index.component';
 import styled from 'styled-components';
 import { SearchIcon } from './components/Icons';
-import { Card } from './components/Card/index.component';
+import { ContinentsList } from './components/Continent/index.component';
 
 const COUNTRIES_FILTERS = ['continent', 'language'];
 
@@ -38,73 +38,48 @@ const Title = styled.h1`
 const INITIAL_DATA = [];
 
 function reducer(data, action) {
-	if (!action.payload.length) return data;
-	data = action.payload;
+	const { payload, changeLoading } = action;
+
 	switch (action.filter) {
 		case 'continent':
-			data.sort(
-				(a, b) => (a.name > b.name && 1) || (a.name < b.name && -1) || 0
-			);
-			data.sort(
-				(a, b) =>
-					(a.continent.name > b.continent.name && 1) ||
-					(a.continent.name < b.continent.name && -1) ||
-					0
-			);
-			action.cahngeLoading();
-			return data;
+			payload.forEach(continent => {
+				continent.countries.sort(
+					(a, b) => (a.name < b.name && -1) || (a.name > b.name && 1) || 0
+				);
+			});
+			changeLoading();
+			return payload;
 		case 'language':
-			data.sort(
-				(a, b) =>
-					(a.languages[0]?.name > b.languages[0]?.name && 1) ||
-					(a.languages[0]?.name < b.languages[0]?.name && -1) ||
-					0
-			);
-			action.cahngeLoading();
-			return data;
-
-		default:
-			return data;
+			payload.forEach(continent => {
+				continent.countries.sort();
+			});
 	}
 }
 
 const App = () => {
 	const [searcherValue, setSearcherValue] = useState('');
 	const [currentFilter, setCurrentFilter] = useState(COUNTRIES_FILTERS[0]);
-	const [filterArr, setFilterArr] = useState([]);
 	const [loading, setLoading] = useState(true);
-	const { data } = useQuery(GET_COUNTRIES);
-	const [countriesState, dispatch] = useReducer(reducer, INITIAL_DATA);
+	const { data } = useQuery(GET_CONTINENTS);
+	const [continents, dispatch] = useReducer(reducer, INITIAL_DATA);
 
 	useEffect(() => {
-		if (!data || !data.countries) return;
-		const arr = [];
-		data.countries.forEach(({ continent }) => {
-			if (!arr.includes(continent.name)) arr.push(continent.name);
-		});
-		setFilterArr(prev => [...prev, ...arr]);
+		if (!data || !data.continents) return;
 		dispatch({
 			filter: currentFilter,
-			payload: data.countries,
-			cahngeLoading: () => setLoading(false),
+			payload: data.continents || [],
+			changeLoading: () => setLoading(false),
 		});
 	}, [data]);
 
 	const handleFilter = filterName => {
 		setCurrentFilter(filterName);
+		setLoading(true);
 	};
 
 	const handleSubmit = e => {
 		e.preventDefault();
 		setLoading(true);
-		dispatch({
-			filter: currentFilter,
-			payload:
-				countriesState && countriesState.length > 0
-					? countriesState
-					: data.countries,
-			cahngeLoading: () => setLoading(false),
-		});
 	};
 
 	const handleChange = e => {
@@ -140,19 +115,7 @@ const App = () => {
 			</FormContainer>
 			<FormContainer as={'div'}>
 				{!loading ? (
-					countriesState.map((v, i) => (
-						<Fragment key={v.name}>
-							{i === 0 ? (
-								<h4>{v.continent.name}</h4>
-							) : countriesState[i - 1].continent.name !==
-							  countriesState[i].continent.name ? (
-								<h4>{countriesState[i].continent.name}</h4>
-							) : (
-								<></>
-							)}
-							<Card countryData={v} />
-						</Fragment>
-					))
+					<ContinentsList continents={continents} />
 				) : (
 					<>Loading...</>
 				)}
