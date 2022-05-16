8300ac9f96afa28d45a1c5a6f722e1183b16b774
IgnFed <ignaciofedorenco2317@gmail.com>
2022-05-15 22:47:27 -0300

    Ahora el componente es funcional

--- kimchechallenge/src/components/Continent/index.component.jsx
+++ kimchechallenge/src/components/Continent/index.component.jsx
@@ -1,7 +1,21 @@
 import React from 'react';
 import { Card } from '../../components/Card/index.component';
 
-export const ContinentsList = ({ continents = [] }) => {
-	console.log(continents);
-	return <></>;
+export const ContinentsList = ({
+	currentFilter = 'continent',
+	countries = [],
+}) => {
+	return (
+		<ul>
+			{countries.length === 0
+				? 'No Countries to display'
+				: countries.map(country => (
+						<Card
+							key={country.name}
+							countryData={country}
+							currentFilter={currentFilter}
+						/>
+				  ))}
+		</ul>
+	);
 };
