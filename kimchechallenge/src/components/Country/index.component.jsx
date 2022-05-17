0515522abe75a44c76a11a253ddcfdb968eeb70d
IgnFed <ignaciofedorenco2317@gmail.com>
2022-05-17 06:32:20 -0300

    Mejor diseño para mostrar el mensaje cunado no hay paises

--- kimchechallenge/src/components/Country/index.component.jsx
+++ kimchechallenge/src/components/Country/index.component.jsx
@@ -1,20 +1,21 @@
 import React from 'react';
 import Card from '../Card/index.component';
 import StyledList from './index.styled';
+import Message from '../Message/index.components';
 
 export default function CountriesList({
 	currentFilter = 'continent',
 	countries = [],
 }) {
+	if (countries.length === 0)
+		return <Message type='info'>No Countries to display</Message>;
 	return (
 		<StyledList>
-			{countries.length === 0
-				? 'No Countries to display'
-				: countries.map(country => (
-						<li key={country.name}>
-							<Card countryData={country} currentFilter={currentFilter} />
-						</li>
-				  ))}
+			{countries.map(country => (
+				<li key={country.name}>
+					<Card countryData={country} currentFilter={currentFilter} />
+				</li>
+			))}
 		</StyledList>
 	);
 }
